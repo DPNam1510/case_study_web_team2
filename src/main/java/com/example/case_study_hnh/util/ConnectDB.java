d639efb1a253edadced303fc633e24e4bf5b9ad2
Ho Ngoc Hieu <youremail@gmail.com>
2025-12-15 23:10:11 +0700

    hieu save code

--- src/main/java/com/example/case_study_hnh/util/ConnectDB.java
+++ src/main/java/com/example/case_study_hnh/util/ConnectDB.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectDB {
     private static final String URL = "jdbc:mysql://localhost:3306/m3_kham_chua_benh";
     private static final String USER = "root";
-    private static final String PASS = "codegym1";
+    private static final String PASS = "codegym";
 
     public static Connection getConnectDB(){
         Connection connection = null;
